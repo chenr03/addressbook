25a0ef863f4a4bc83ab0404ba4f27368eabedae9
Chris Henry <chrislouishenry@gmail.comgit statuscd~cd>
2022-10-26 20:01:18 -0500

    beginning to change back to functional based components

--- src/App.js
+++ src/App.js
@@ -40,51 +40,51 @@ console.clear();
 
 
 
-export default class App extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            randomUsers: [],
-            userIsLoggedIn: false
-        };
-    }
-
-
-    componentDidMount() {
-        fetch("https://randomuser.me/api?results=25")
-            .then((response) => response.json())
-            .then((data) => {
-                console.log(data)
-                this.setState({
-                    randomUsers: data.results
-                })
-            });
-    }
-
-    componentDidUpdate() {
-        console.log(this.state.randomUsers)
-    }
-
-    render() {
-        return (
-
-            <div className="App">
-                <ul>
-                    {this.state.randomUsers.map((randomUsers) => {
-                        return (
-                            <li>
-                                <img src={randomUsers.picture.large} alt={randomUsers.name}/>
-                                <h1>{randomUsers.name.first} {randomUsers.name.last}</h1>
-
-
-                            </li>
-                        );
-                    })}
-                </ul>
-            </div>
-        );
-    }
-}
+// export default class App extends React.Component {
+//     constructor(props) {
+//         super(props);
+//
+//         this.state = {
+//             randomUsers: [],
+//             userIsLoggedIn: false
+//         };
+//     }
+//
+//
+//     componentDidMount() {
+//         fetch("https://randomuser.me/api?results=25")
+//             .then((response) => response.json())
+//             .then((data) => {
+//                 console.log(data)
+//                 this.setState({
+//                     randomUsers: data.results
+//                 })
+//             });
+//     }
+//
+//     componentDidUpdate() {
+//         console.log(this.state.randomUsers)
+//     }
+//
+//     render() {
+//         return (
+//
+//             <div className="App">
+//                 <ul>
+//                     {this.state.randomUsers.map((randomUsers) => {
+//                         return (
+//                             <li>
+//                                 <img src={randomUsers.picture.large} alt={randomUsers.name}/>
+//                                 <h1>{randomUsers.name.first} {randomUsers.name.last}</h1>
+//
+//
+//                             </li>
+//                         );
+//                     })}
+//                 </ul>
+//             </div>
+//         );
+//     }
+// }
 
 
